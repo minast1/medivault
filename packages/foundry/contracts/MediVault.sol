ec161943d6bd842a2f87e8b4d308da7791beac50
minast1 <edmarfo2@gmail.com>
2026-03-24 18:18:53 +0000

    feat: medivault contract implementation

--- packages/foundry/contracts/MediVault.sol
+++ packages/foundry/contracts/MediVault.sol
@@ -1,12 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "forge-std/console.sol";
-
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
 /**
  * @title MediVault
  * Health data and sovereign vault
